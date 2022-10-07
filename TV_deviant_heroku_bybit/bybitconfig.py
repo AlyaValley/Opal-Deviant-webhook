449afba1827d715db4d3d9b3d461b117249513b6
samuelCollombet <115131765+AlyaValley@users.noreply.github.com>
2022-10-07 00:12:56 -0700

    OPAL

--- TV_deviant_heroku_bybit/bybitconfig.py
+++ TV_deviant_heroku_bybit/bybitconfig.py
@@ -1,8 +1,8 @@
 #Passphrase !!!!Keep it for you!!!!!!!
-WEBHOOK_PASSPHRASE = "change" #A Modifier | garder les >""
+WEBHOOK_PASSPHRASE = "YOUR PASSPHRASE" #A Modifier | garder les >""
 
 #API Keys bybit !!!!Keep them for you!!!!!!!
-BYBIT_API_KEY = "change"
-BYBIT_API_SECRET = "change"
+BYBIT_API_KEY = "your key"
+BYBIT_API_SECRET = "your secret key"
 
 
